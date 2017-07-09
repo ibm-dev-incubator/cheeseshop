b060ae6f52b957f39f42ba0191a2122f2531d829
Gregory Haynes <greg@greghaynes.net>
2017-07-09 02:06:02 +0000

    Add create/get replay in dbapi

--- cheeseshop/dbapi.py
+++ cheeseshop/dbapi.py
@@ -14,3 +14,26 @@ async def create_schema(conn):
         CREATE UNIQUE INDEX ON replays (sha1sum)
     ''')
 
+
+class Replay(object):
+    def __init__(self, id_, sha1sum, filename):
+        self.id = id_
+        self.sha1sum = sha1sum
+        self.filename = filename
+
+
+class DbApi(object):
+    def __init__(self, conn):
+        self.conn = conn
+
+    async def create_replay(self, sha1sum, filename):
+        await self.conn.execute('''
+            INSERT INTO replays(sha1sum, filename)
+            VALUES($1, $2)
+        ''', sha1sum, filename)
+
+    async def get_replay_by_sha1sum(self, sha1sum):
+        row = await self.conn.fetchrow('''
+            SELECT * FROM replays WHERE sha1sum = $1
+        ''', sha1sum)
+        return Replay(row['id'], row['sha1sum'], row['filename'])
