80e42050fa8ba7a79c3bd69efaf757563aa3fd4f
Gregory Haynes <greg@greghaynes.net>
2017-07-18 19:01:02 +0000

    WIP for streaming GSI events

--- cheeseshop/games/csgo.py
+++ cheeseshop/games/csgo.py
@@ -1,4 +1,5 @@
 import asyncio
+import collections
 from concurrent.futures import FIRST_COMPLETED
 import json
 
@@ -7,11 +8,13 @@ import aiohttp_jinja2
 
 from cheeseshop.games import gameapi
 
+
 class GsiPlayer(object):
-    def __init__(self, request, gsi_id):
+    def __init__(self, request, streamer_id):
         self._request = request
-        self._gsi_id = gsi_id
+        self._streamer_id = streamer_id
         self._ws = None
+        self._event_queue = asyncio.Queue(maxsize=20)
 
     async def handle(self):
         self._ws = web.WebSocketResponse()
@@ -27,10 +30,12 @@ class GsiPlayer(object):
 
         return self._ws
 
+    async def handle_event(self, gsi_event):
+        await self._event_queue.put(gsi_event)
+
     async def _send(self):
         while True:
-            self._ws.send_str('hello')
-            asyncio.sleep(1)
+            self._ws.send_json(await self._event_queue.get())
 
     async def _listen(self):
         async for msg in self._ws:
@@ -38,6 +43,10 @@ class GsiPlayer(object):
 
 
 class CsGoApi(gameapi.GameApi):
+    def __init__(self, config, sql_pool):
+        super(CsGoApi, self).__init__(config, sql_pool)
+        self._gsi_players = collections.defaultdict(list)
+
     def add_routes(self, router):
         router.add_post('/games/csgo/gsi/{streamer_id}/input',
                         self._handle_input_gsi)
@@ -46,7 +55,10 @@ class CsGoApi(gameapi.GameApi):
 
     @aiohttp_jinja2.template('get_upload.html')
     async def _handle_input_gsi(self, request):
+        streamer_id = request.match_info.get('streamer_id')
         gsi_data = await request.json()
+        for player in self._gsi_players[streamer_id]:
+            await player.handle_event(gsi_data)
         print()
         print()
         print(json.dumps(gsi_data, indent=4, sort_keys=True))
@@ -56,5 +68,10 @@ class CsGoApi(gameapi.GameApi):
         return {}
 
     async def _handle_play_gsi(self, request):
-        gsi_id = request.match_info.get('gsi_id')
-        return await GsiPlayer(request, gsi_id).handle()
+        streamer_id = request.match_info.get('streamer_id')
+        player = GsiPlayer(request, gsi_id)
+        try:
+            self._gsi_players[streamer_id].append(player)
+            return await player.handle()
+        finally:
+            self._gsi_players[streamer_id].remove(player)
